a36236bc78d295a8f0a10fb5a18df56d7eb25630
Rogerio Barreira <abacuque37@hotmail.com>
2023-05-30 22:22:14 -0300

    Coordinator implemented

---
+++ PokemonFull/Source/Controllers/PokemonList/PokemonListView.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonListView.swift
+//  PokemonFull
+//
+//  Created by Rogerio Martins on 30/05/23.
+//
+
+import UIKit
+
+class PokemonListView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
